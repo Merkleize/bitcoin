617050a073ef7f7a0e241b8c2f700e90ed81bf4c
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-03-01 22:48:23 +0100

    consensus: Add OP_CHECKCONTRACTVERIFY

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -269,22 +269,43 @@ struct ScriptExecutionData
 
     //! The taproot internal key. */
     std::optional<XOnlyPubKey> m_internal_key = std::nullopt;
+
+    //! Whether m_taproot_merkle_root is initialized.
+    bool m_taproot_merkle_root_init = false;
+    //! The merkle root of the taproot tree.
+    uint256 m_taproot_merkle_root;
+
+    //! Whether m_ccv_amount is initialized.
+    bool m_ccv_amount_init = false;
+    //! Residual amount of the current input according to CHECKCONTRACTVERIFY semantics.
+    CAmount m_ccv_amount;
 };
 
 /** The state of the script interpreter that persists across inputs of a single transaction.
  *
  * As access happens across different worker threads, it is crucial that access to this struct
- * is properly synchronized. Code accessing its members must hold the m_mutex lock. */
+ * is properly synchronized. Code accessing its members must hold the m_mutex lock.
+ * Currently only used for OP_CHECKCONTRACTVERIFY. */
 struct TransactionExecutionData {
     const CTransaction* m_tx;
     std::mutex m_mutex;
 
-    // members related to the validation state of this transaction
-    // that need to persist across inputs will be added here,
-    // protected by m_mutex.
+    // For each output, the minimum amount of that output in order for the transaction
+    // to be considered valid. Accumulated during the evaluation of OP_CHECKCONTRACTVERIFY
+    // with the 'default' semantics.
+    std::vector<CAmount> m_ccv_output_min_amount GUARDED_BY(m_mutex);
+    // Set to true if this output has been checked with OP_CHECKCONTRACTVERIFY
+    // with the 'default' semantics.
+    std::vector<bool> m_ccv_output_checked_default GUARDED_BY(m_mutex);
+    // Set to true if this output has been checked with OP_CHECKCONTRACTVERIFY
+    // with the 'deduct' semantics.
+    std::vector<bool> m_ccv_output_checked_deduct GUARDED_BY(m_mutex);
 
     TransactionExecutionData(const CTransaction* tx)
-        : m_tx(tx)
+        : m_tx(tx),
+          m_ccv_output_min_amount(tx ? tx->vout.size() : 0, 0),
+          m_ccv_output_checked_default(tx ? tx->vout.size() : 0, false),
+          m_ccv_output_checked_deduct(tx ? tx->vout.size() : 0, false)
     {
         assert(tx != nullptr); // Ensure the transaction pointer is valid
     }
@@ -363,6 +384,11 @@ public:
         return false;
     }
 
+    virtual bool CheckContract(int flags, int index, const std::vector<unsigned char>& pubkey, const std::vector<unsigned char>& data, const std::vector<unsigned char>& taptree, ScriptExecutionData& ScriptExecutionData, ScriptError* serror, TransactionExecutionData* tx_exec_data) const
+    {
+        return false;
+    }
+
     virtual ~BaseSignatureChecker() = default;
 };
 
@@ -400,6 +426,7 @@ public:
     bool CheckLockTime(const CScriptNum& nLockTime) const override;
     bool CheckSequence(const CScriptNum& nSequence) const override;
     bool CheckDefaultCheckTemplateVerifyHash(const Span<const unsigned char>& hash) const override;
+    bool CheckContract(int flags, int index, const std::vector<unsigned char>& pubkey, const std::vector<unsigned char>& data, const std::vector<unsigned char>& taptree, ScriptExecutionData& ScriptExecutionData, ScriptError* serror, TransactionExecutionData* tx_exec_data) const override;
 };
 
 using TransactionSignatureChecker = GenericTransactionSignatureChecker<CTransaction>;
